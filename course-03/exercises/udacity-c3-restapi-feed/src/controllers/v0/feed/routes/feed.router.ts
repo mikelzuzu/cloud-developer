48ade1a7b4d92009027929376a4815cf3f1f7faa
mikelzuzu <mikelzuzu@gmail.com>
2020-04-20 09:07:51 +0100

    added authentication in Fedd

--- course-03/exercises/udacity-c3-restapi-feed/src/controllers/v0/feed/routes/feed.router.ts
+++ course-03/exercises/udacity-c3-restapi-feed/src/controllers/v0/feed/routes/feed.router.ts
@@ -1,8 +1,31 @@
 import { Router, Request, Response } from 'express';
 import { FeedItem } from '../models/FeedItem';
 import * as AWS from '../../../../aws';
+import { NextFunction } from 'connect';
+import * as jwt from 'jsonwebtoken';
+import { config } from '../../../../config/config';
 
 const router: Router = Router();
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+    //return next();
+    if (!req.headers || !req.headers.authorization){
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
+    
+    const token_bearer = req.headers.authorization.split(' ');
+    if(token_bearer.length != 2){
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
+    
+    const token = token_bearer[1];
+    return jwt.verify(token, config.jwt.secret, (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+      }
+      return next();
+    });
+}
 
 // Get all feed items
 router.get('/', async (req: Request, res: Response) => {
@@ -13,7 +36,7 @@ router.get('/', async (req: Request, res: Response) => {
 
         if (!items) {
             return res.status(404)
-                    .send(`Item not found for given ID`);
+                    .send(`Item not found for given ID: ${id}`);
         }
     } else {
         items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
@@ -47,7 +70,7 @@ router.get('/:id', async (req: Request, res: Response) => {
 
 // update a specific resource
 router.patch('/:id', 
-    //requireAuth, 
+    requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
         res.send(500).send("not implemented")
@@ -56,7 +79,7 @@ router.patch('/:id',
 
 // Get a signed url to put a new item in the bucket
 router.get('/signed-url/:fileName', 
-    //requireAuth, 
+    requireAuth, 
     async (req: Request, res: Response) => {
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
@@ -67,7 +90,7 @@ router.get('/signed-url/:fileName',
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
 router.post('/', 
-    //requireAuth, 
+    requireAuth, 
     async (req: Request, res: Response) => {
     const caption = req.body.caption;
     const fileName = req.body.url;
