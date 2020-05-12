43dd0489b901c627cce9bc62ded8ded58e6dc855
mikelzuzu <mikelzuzu@gmail.com>
2020-05-12 09:39:34 +0100

    incluse Auth0 authorizer

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -6,13 +6,15 @@ import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import { buildCert } from '../../auth/utils'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+
+const jwksUrl = process.env.JWKS_URL
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +60,19 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
+  //Check the algorithm in the token
+  if (jwt.header.alg != 'RS256') throw new Error('Not supported algorithm for the token')
+
+  //get the jwks
+  const jwks = await getKeySet()
+  //get the key that we need
+  const signingKey = getSigningKey(jwt.header.kid, jwks)
+  const cert = buildCert(signingKey.x5c[0])
+
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -75,3 +86,14 @@ function getToken(authHeader: string): string {
 
   return token
 }
+
+async function getKeySet() {
+  const response = await Axios.get(jwksUrl)
+
+  return response.data.keys
+}
+
+function getSigningKey(kid: string, keySet) {
+  return keySet.find(key => key.kid === kid)
+}
+
