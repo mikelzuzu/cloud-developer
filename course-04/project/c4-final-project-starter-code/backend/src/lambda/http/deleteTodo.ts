2c5a99386f2ce82bd7d20ebc4875992fc5be1e6e
mikelzuzu <mikelzuzu@gmail.com>
2020-05-12 11:56:35 +0100

    added delete functionality

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,46 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getUserId } from '../utils'
+import { deleteTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('deleteTodo')
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('Processing event: ', event)
+  
   const todoId = event.pathParameters.todoId
 
   // TODO: Remove a TODO item by id
-  return undefined
-}
+
+  const userId = getUserId(event)
+
+  try {
+    await deleteTodo(todoId, userId)
+    //HTTP 200 or HTTP 204 should imply "resource deleted successfully"
+    //204 (No Content)
+    return {
+      statusCode: 204,
+      body: ''
+    }
+  } catch (error) {
+    // send back http 404 not found error
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        error: error.message
+      })
+    }
+  }
+
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
