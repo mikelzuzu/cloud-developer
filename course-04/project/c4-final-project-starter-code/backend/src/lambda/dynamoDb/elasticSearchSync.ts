53ac5d6c37fe29e6229beadd6d8111773eeec18a
mikelzuzu <mikelzuzu@gmail.com>
2020-05-16 20:37:52 +0100

    fix creation of body

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/dynamoDb/elasticSearchSync.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/dynamoDb/elasticSearchSync.ts
@@ -8,38 +8,41 @@ const esHost = process.env.ES_ENDPOINT
 const logger = createLogger('getTodos')
 
 const es = new elasticsearch.Client({
-  hosts: [ esHost ],
-  connectionClass: httpAwsEs
+    hosts: [esHost],
+    connectionClass: httpAwsEs
 })
 
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
-  logger.info('Processing events batch from DynamoDB', { event:JSON.stringify(event) })
+    logger.info('Processing events batch from DynamoDB', { event: JSON.stringify(event) })
 
-  for (const record of event.Records) {
-    logger.debug('Processing record', { item:JSON.stringify(record) })
-    if (record.eventName !== 'INSERT') {
-      continue
+    for (const record of event.Records) {
+
+        if (record.eventName !== 'INSERT') {
+            continue
+        }
+        logger.info('Processing record', { item: JSON.stringify(record) })
+        const newItem = record.dynamodb.NewImage
+
+        const todoId = newItem.todoId.S
+
+        const body = {
+            todoId: newItem.todoId.S,
+            userId: newItem.userId.S,
+            name: newItem.name.S,
+            dueDate: newItem.dueDate.S,
+            createdAt: newItem.createdAt.S
+        }
+
+        try {
+            await es.index({
+                index: 'todos-index',
+                type: 'todos',
+                id: todoId,
+                body
+            })
+        } catch (error) {
+            logger.error('Error sending data to elasticsearch.', { errorMessage: error.message })
+        }
+
     }
-
-    const newItem = record.dynamodb.NewImage
-
-    const todoId = newItem.todoId.S
-
-    const body = {
-        todoId: newItem.imageId.S,
-        userId: newItem.groupId.S,
-        attachmentUrl: newItem.imageUrl.S,
-        name: newItem.title.S,
-        createdAt: newItem.timestamp.S,
-        done: newItem.done.S
-      }
-
-    await es.index({
-      index: 'todos-index',
-      type: 'todos',
-      id: todoId,
-      body
-    })
-
-  }
 }
